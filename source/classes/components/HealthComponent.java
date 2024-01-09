50e84170c565cc4d3f7b24cfbdacd468ad917f85
Shengalabu <shengalabu@gmail.com>
2024-01-09 17:36:59 +0800

    Main branch me

--- source/classes/components/HealthComponent.java
+++ source/classes/components/HealthComponent.java
@@ -3,8 +3,6 @@ package source.classes.components;
 import source.classes.base_classes.ActorComponent;
 
 public class HealthComponent extends ActorComponent{
-
-public class HealthComponent extends Object{
     float Health = 100;
 
     //Returns the health of the component
