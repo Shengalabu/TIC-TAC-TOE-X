bf2445f189459ecdc5888a84ad9fa9c96b2c965c
Shengalabu <shengalabu@gmail.com>
2024-01-09 18:37:23 +0800

    GameInstanceChange

--- source/Main.java
+++ source/Main.java
@@ -1,8 +1,28 @@
 package source;
 
 public class Main {
+
+    public static void clearConsole() {
+        try {
+            // Clear console command for Windows, Unix, and Mac
+            String os = System.getProperty("os.name").toLowerCase();
+
+            if (os.contains("win")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            // Handle exceptions
+            e.printStackTrace();
+        }
+    }
+    
     public static void main(String[] args) {
         GameInstance activeGameInstance = new GameInstance();
         activeGameInstance.Run();
+        System.out.println("HELLO");
+        clearConsole();
+        
     }
 }
--- source/classes/base_classes/GameInstance.java
+++ source/classes/base_classes/GameInstance.java
@@ -1,8 +1,9 @@
 package source;
 
+import source.classes.base_classes.Actor;
 import source.classes.player.PlayerPawn;
 
-public class GameInstance extends Object{
+public class GameInstance extends Actor{
     
     public void Run(){
         PlayerPawn playerInst = new PlayerPawn();
